f3ac8604d2386201717b6c5930208707fd9e4702
rr- <rr-@sakuya.pl>
2019-07-05 18:58:31 +0200

    refactor

---
+++ oc_stats/data/report-daily-stats.js
@@ -0,0 +1,96 @@
+var parseTime = d3.timeParse('%Y-%m-%d');
+hits.forEach(d => d.day = parseTime(d.day));
+downloads.forEach(d => d.day = parseTime(d.day));
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    var margin = {top: 10, right: 10, bottom: 20, left: 50};
+    var outerWidth = 800;
+    var outerHeight = 300;
+    var innerWidth = outerWidth - margin.left - margin.right;
+    var innerHeight = outerHeight - margin.top - margin.bottom;
+
+    var x = d3.scaleTime()
+        .range([0, innerWidth])
+        .domain(d3.extent(hits.concat(downloads), d => d.day));
+    var y = d3.scaleLinear()
+        .range([innerHeight, 0])
+        .domain([
+            0,
+            d3.max([...hits, ...downloads], d => Math.min(2000, d.value))
+        ])
+        .nice();
+
+    var svg = d3
+        .select('.daily-stats>.target')
+        .classed('svg-container', true)
+        .append('svg')
+        .attr('viewBox', `0 0 ${outerWidth} ${outerHeight}`)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // grid
+    svg.append('g')
+        .attr('class', 'grid')
+        .attr('transform', `translate(0,${innerHeight})`)
+        .call(d3.axisBottom(x)
+            .ticks(d3.timeMonth.every(1))
+            .tickSize(-innerHeight)
+            .tickFormat('')
+        )
+    svg.append('g')
+        .attr('class', 'grid')
+        .call(d3.axisLeft(y)
+            .tickSize(-innerWidth)
+            .tickFormat('')
+        )
+
+    for (const chart of [
+        {data: hits, className: 'hits'},
+        {data: downloads, className: 'downloads'},
+    ]) {
+        // areas
+        svg.append('path')
+            .data([chart.data])
+            .attr('class', `area ${chart.className}`)
+            .attr(
+                'd',
+                d3.area()
+                .x(d => x(d.day))
+                .y0(innerHeight)
+                .y1(d => y(d.value))
+            );
+
+        // lines
+        svg.append('path')
+            .data([chart.data])
+            .attr('class', `line ${chart.className}`)
+            .attr(
+                'd',
+                d3.line()
+                .x(d => x(d.day))
+                .y(d => y(d.value))
+            );
+
+        // average lines
+        svg.append('path')
+            .data([chart.data])
+            .attr('class', `line ${chart.className}-avg`)
+            .attr(
+                'd',
+                d3.line()
+                .x(d => x(d.day))
+                .y(d => y(d.value_avg))
+            );
+    }
+
+    // axes
+    svg.append('g')
+        .attr('transform', `translate(0,${innerHeight})`)
+        .call(
+            d3.axisBottom(x)
+            .ticks(d3.timeMonth.every(3))
+        );
+    svg.append('g')
+        .call(d3.axisLeft(y));
+});
