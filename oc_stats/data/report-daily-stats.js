41697757e936462b1dbe472ac414bd35a28eef46
rr- <rr-@sakuya.pl>
2019-07-06 23:55:15 +0200

    add legend

--- oc_stats/data/report-daily-stats.js
+++ oc_stats/data/report-daily-stats.js
@@ -1,19 +1,19 @@
-var parseTime = d3.timeParse('%Y-%m-%d');
+const parseTime = d3.timeParse('%Y-%m-%d');
 hits.forEach(d => d.day = parseTime(d.day));
 downloads.forEach(d => d.day = parseTime(d.day));
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    var margin = {top: 10, right: 10, bottom: 20, left: 50};
-    var outerWidth = 800;
-    var outerHeight = 300;
-    var innerWidth = outerWidth - margin.left - margin.right;
-    var innerHeight = outerHeight - margin.top - margin.bottom;
+    const margin = {top: 10, right: 10, bottom: 20, left: 50};
+    const outerWidth = 800;
+    const outerHeight = 300;
+    const innerWidth = outerWidth - margin.left - margin.right;
+    const innerHeight = outerHeight - margin.top - margin.bottom;
 
-    var x = d3.scaleTime()
+    const x = d3.scaleTime()
         .range([0, innerWidth])
         .domain(d3.extent(hits.concat(downloads), d => d.day));
-    var y = d3.scaleLinear()
+    const y = d3.scaleLinear()
         .range([innerHeight, 0])
         .domain([
             0,
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
         ])
         .nice();
 
-    var svg = d3
+    const svg = d3
         .select('.daily-stats>.target')
         .classed('svg-container', true)
         .append('svg')
@@ -37,18 +37,20 @@ window.addEventListener('DOMContentLoaded', () => {
             .ticks(d3.timeMonth.every(1))
             .tickSize(-innerHeight)
             .tickFormat('')
-        )
+        );
     svg.append('g')
         .attr('class', 'grid')
         .call(d3.axisLeft(y)
             .tickSize(-innerWidth)
             .tickFormat('')
-        )
+        );
 
-    for (const chart of [
-        {data: hits, className: 'hits'},
-        {data: downloads, className: 'downloads'},
-    ]) {
+    const series = [
+        {data: hits, title: 'Website hits', className: 'hits'},
+        {data: downloads, title: 'Downloads', className: 'downloads'},
+    ];
+
+    for (const chart of series) {
         // areas
         svg.append('path')
             .data([chart.data])
@@ -83,6 +85,40 @@ window.addEventListener('DOMContentLoaded', () => {
                 .y(d => y(d.diff_avg))
             );
     }
+
+    // legend
+    const legendSquareSize = 12;
+    const legendSquareSpacing = 8;
+    const legendTextSpacing = 5;
+    const legendPadding = 8;
+    const legendX = 12;
+    const legendY = 12;
+
+    svg.append('rect')
+        .attr('x', legendX)
+        .attr('y', legendY)
+        .attr('width', 95)
+        .attr('height', 2 * legendPadding + legendSquareSize * series.length + legendSquareSpacing * (series.length - 1))
+        .attr('class', 'legend-wrapper');
+
+    svg.selectAll("legendSquares")
+        .data(series)
+        .enter()
+        .append("rect")
+        .attr("x", legendX + legendPadding)
+        .attr("y", (d, i) => legendY + legendPadding + i * (legendSquareSize + legendSquareSpacing))
+        .attr("width", legendSquareSize)
+        .attr("height", legendSquareSize)
+        .attr("class", d => `legend ${d.className}`);
+
+    svg.selectAll("legendLabels")
+        .data(series)
+        .enter()
+        .append("text")
+        .attr("x", legendX + legendPadding + legendSquareSize + legendTextSpacing)
+        .attr("y", (d, i) => legendY + legendPadding + i * (legendSquareSize + legendSquareSpacing) + (legendSquareSize / 2))
+        .attr("class", d => `legend ${d.className}`)
+        .text(d => d.title);
 
     // axes
     svg.append('g')
